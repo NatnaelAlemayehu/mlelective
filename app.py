b310525a77734c7cd64903feec30f32cd6f33ef5
Natnael <35394453+NatnaelAlemayehu@users.noreply.github.com>
2021-10-22 08:52:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 
 # Create flask app
-flask_app = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open("finalized_model.pkl", "rb"))
 
-@flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("index.html")
 
-@flask_app.route("/predict", methods = ["POST"])
+@app.route("/predict", methods = ["POST"])
 def predict():
     float_features = [int(x) for x in request.form.values()]
     print(float_features)
@@ -19,4 +19,4 @@ def predict():
     return render_template("index.html", prediction_text = "The overall score is {}".format(prediction))
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
